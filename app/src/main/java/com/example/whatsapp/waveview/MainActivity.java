85a9a7eab609602ad2793d2bad5a6c5114dc73d3
subhatechtree <subhatechtree@gmail.com>
2017-08-29 12:03:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whatsapp/waveview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.whatsapp.waveview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.john.waveview.WaveView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WaveView waveView= (WaveView) findViewById(R.id.waveView);
+        SeekBar seekBar= (SeekBar) findViewById(R.id.seekbar);
+        waveView.setProgress(seekBar.getProgress());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+             waveView.setProgress(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
